6914909432e304caba8abc2a302501b245537ce7
Topher <topher@tri.be>
2021-06-09 19:31:10 -0400

    Hide info link properly, remove links

--- classes/class-beverage-table-shortcode.php
+++ classes/class-beverage-table-shortcode.php
@@ -128,9 +128,9 @@ class Beverage_Table_Shortcode {
 				$tags          = implode( ', ', wp_list_pluck( $tags_array, 'name' ) );
 
 				$output .= '<tr>' . "\n";
-				$output .= '<td nowrap><a href="#TB_inline?&width=600&height=550&inlineId=beverage-' . get_the_ID() . '" class="thickbox"><span class="dashicons dashicons-info-outline"></span> <span class="hidden">Info</span></a></td>' . "\n";
-				$output .= '<td><a href="' . esc_url( get_post_meta( get_the_ID(), 'beverage_link', true ) ) . '">' . get_the_title() . '</a></td>' . "\n";
-				$output .= '<td><a href="' . esc_url( get_term_meta( $vendors_array[0]->term_id, 'beverage_vendor_link', true ) ) . '">' . esc_html( $vendors_array[0]->name ) . '</a></td>' . "\n";
+				$output .= '<td nowrap align="center"><a href="#TB_inline?&width=600&height=550&inlineId=beverage-' . get_the_ID() . '" class="thickbox"><span class="dashicons dashicons-info-outline"></span> <span class="screen-reader-text">Info</span></a></td>' . "\n";
+				$output .= '<td>' . get_the_title() . '</td>' . "\n";
+				$output .= '<td>' . esc_html( $vendors_array[0]->name ) . '</td>' . "\n";
 				$output .= '<td>' . esc_html( get_post_meta( get_the_ID(), 'beverage_currency', true ) . get_post_meta( get_the_ID(), 'beverage_price', true ) ) . '</td>' . "\n";
 				$output .= '<td nowrap>' . eh_cmb2_get_star_rating_field( 'beverage_rating', get_the_ID() ) . '</td>' . "\n";
 				$output .= '<td>';
@@ -140,7 +140,7 @@ class Beverage_Table_Shortcode {
 
 				$output .= '<h3>' . get_the_title() . '</h3>';
 
-				$output .= get_the_post_thumbnail( get_the_id(), array( 100, 500 ), array( 'class' => 'alignright' ) );
+				$output .= get_the_post_thumbnail( get_the_id(), array( 200, 500 ), array( 'class' => 'alignright' ) );
 
 				$output .= get_the_content();
 
